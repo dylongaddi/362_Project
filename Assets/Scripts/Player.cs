bc096d3f59327d2a63984e1764ef915b05755ace
dylongaddi <dylongaddi@gmail.com>
2023-11-06 15:35:01 -0800

    created bullet prefab and bullet shooting script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Bullet bulletPrefab;
     public float thrustSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
 
     private Rigidbody2D _rigidBody;
-    private float _turnDirection;
+
     private bool _thrusting;
+
+    private float _turnDirection;
+    
 
     private void Awake()
     {
@@ -21,12 +26,18 @@ public class Player : MonoBehaviour
         _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
 
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            _turnDirection = -1.0f;
+            _turnDirection = 1.0f;
         } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) { 
             _turnDirection = -1.0f;
         } else {
             _turnDirection = 0.0f;
         }       
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
     }
 
     private void FixedUpdate()
@@ -35,5 +46,18 @@ public class Player : MonoBehaviour
         {
             _rigidBody.AddForce(this.transform.up * this.thrustSpeed);
         }
+
+        if (_turnDirection != 0.0f)
+        {
+            _rigidBody.AddTorque(_turnDirection * this.turnSpeed);
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
     }
 }
+
+    
